1b0676c7c0c4e77cb3889009d2a33b6440e1ff8b
李勇 <yong.li@yidatec.com>
2026-04-25 19:32:20 +0800

    feat: 链接数据库

---
+++ demo-go/db/mysql.go
@@ -0,0 +1,17 @@
+package db
+
+import (
+	"gorm.io/gorm",
+	"gorm.io/driver/sqlite"
+)
+
+func init() {
+	// 这里可以放置数据库连接的初始化代码，例如连接 MySQL 数据库。
+
+	dsn := "user:pass@tcp(127.0.0.1:3306)/demo-01?charset=utf8mb4&parseTime=True&loc=Local"
+
+	_, err := gorm.Open(sqlite.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("连接数据库失败")
+	}
+}
